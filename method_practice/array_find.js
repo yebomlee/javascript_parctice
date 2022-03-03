d1f9673f7554bf9f8a1a1ec306939ccaeeb75ac4
yebomlee <dev.yespring@gmail.com>
2022-03-03 10:17:10 +0900

    Add: array.prototype.find()

---
+++ method_practice/array_find.js
@@ -0,0 +1,13 @@
+// find(): 검사를 위해 전달받은 함수를 만족하는 배열의 첫 번째 요소의 값을 반환함. 만족하는 값이 없으면 undefined를 반환함.
+
+// 배열 numbers에 5가 포함되어 있는지 find 이용하기.
+const numbers = [1, 2, 3, 4, 5];
+
+function callBackFn(ele, idx, arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (ele === 5) return true;
+  }
+}
+
+let result = numbers.find(callBackFn);
+console.log('결과', result); // 5
