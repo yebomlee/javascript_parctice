68e32a098c165200ffa55b4d7a156840c7b63d1c
yebomlee <dev.yespring@gmail.com>
2022-03-04 16:12:04 +0900

    Add: Array.some()

---
+++ method_practice/array_some.js
@@ -0,0 +1,17 @@
+// some(): 배열 요소가 테스트를 통과하는지 확인.
+
+// 객체의 value에 null이 하나라도 있으면 순회를 멈추고 "400 ERROR"를 출력.
+function solution(element, index, array) {
+  if (element === null) {
+    console.log('400 ERROR');
+    return;
+  }
+}
+
+const inputs = {
+  name: '코캠',
+  contents: '안녕하세요.',
+  title: null,
+};
+
+Object.values(inputs).some(solution);
