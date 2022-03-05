24906eaacc91f00d8ac09b663eba980b8dd80864
yebomlee <dev.yespring@gmail.com>
2022-03-05 15:06:38 +0900

    Modify: array_fill.js and add Array.prototype.fill() method example

--- method_practice/array_fill.js
+++ method_practice/array_fill.js
@@ -8,3 +8,11 @@ function solution(phone) {
 }
 
 console.log('결과', solution('01012345678')); // "0101234****"
+
+// 주어진 배열의 2번째부터 5번째까지 숫자 10으로 변경해 주는 함수를 만들기.
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+function fill(arr) {
+  return arr.fill(10, 1, 5);
+}
+console.log(fill(arr)); // [1, 10, 10, 10, 10, 6, 7, 8]
